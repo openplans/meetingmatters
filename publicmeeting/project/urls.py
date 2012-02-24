56772e634ace80bff877a22d72f206e0175fc3c3
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-02-24 14:33:06 -0500

    Add a simple layout file (Bootstrap)

--- publicmeeting/project/urls.py
+++ publicmeeting/project/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from django.views.generic import TemplateView
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', TemplateView.as_view(template_name='project-home.html')),
 )
