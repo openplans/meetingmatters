e28c5ba97eb6ea25ff09eb3fd457446521573a97
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-02-27 13:46:08 -0500

    Start adding the ability to view meeting detail

--- publicmeeting/project/urls.py
+++ publicmeeting/project/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 from django.views.generic import TemplateView
 
+from . import views
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -21,6 +23,11 @@ urlpatterns = patterns('',
     url(r'', include('django.contrib.auth.urls')),
 
     url(r'^meetings/', include('create_meeting.urls')),
+
+    url(r'^meetings/$', views.MeetingListView.as_view(),
+        name='browse_meetings_meeting_list'),
+    url(r'^meetings/(?P<slug>.+)$', views.MeetingDetailView.as_view(),
+        name='browse_meetings_meeting_detail'),
 
     url(r'^$', TemplateView.as_view(template_name='project-home.html')),
 )
--- publicmeeting/project/views.py
+++ publicmeeting/project/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.views import generic as views
+
+from . import models
+
+class MeetingListView (views.ListView):
+    model = models.Meeting
+    context_object_name = 'meetings'
+    template_name = 'browse_meetings-meeting_list.html'
+
+
+class MeetingDetailView (views.DetailView):
+    model = models.Meeting
+    context_object_name = 'meeting'
+    template_name = 'browse_meetings-meeting_detail.html'
