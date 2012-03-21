6bda4305a28fbc7f922d02b5b07ad3c4bc7d7abf
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-03-21 18:31:16 -0400

    Toggle topics on and off

--- publicmeeting/meetings/views/browse.py
+++ publicmeeting/meetings/views/browse.py
@@ -13,20 +13,35 @@ class MeetingListView (views.ListView):
         context = super(MeetingListView, self).get_context_data(**kwargs)
 
         tag_slugs = self.request.GET.getlist('tags')
-        context['tags'] = taggit_models.Tag.objects.all().order_by('name')
-        context['selected_tags'] = taggit_models.Tag.objects.filter(slug__in=tag_slugs)
+        all_tags = taggit_models.Tag.objects.all().order_by('name')
+        selected_tags = taggit_models.Tag.objects.filter(slug__in=tag_slugs)
+
+        context['tags'] = all_tags
+        context['selected_tags'] = selected_tags
 
         context['rss_url'] = reverse('meeting_list_rss') + '?' + self.request.GET.urlencode()
         context['ical_url'] = reverse('meeting_list_ical') + '?' + self.request.GET.urlencode()
 
+        for tag in all_tags:
+            get_params = self.request.GET.copy()
+            tag_slugs_copy = tag_slugs[:]
+            if tag in selected_tags:
+                tag_slugs_copy.remove(tag.slug)
+            else:
+                tag_slugs_copy.append(tag.slug)
+            get_params.setlist('tags', tag_slugs_copy)
+            tag.qs = get_params.urlencode()
+
         return context
 
     def get_queryset(self):
-        queryset = models.Meeting.objects.all().order_by('-begin_time')
+        queryset = models.Meeting.objects.all().order_by('-begin_time').select_related()
         tag_slugs = self.request.GET.getlist('tags')
 
         if tag_slugs:
-            queryset = queryset.filter(tags__slug__in=tag_slugs).distinct()
+            for tag_slug in tag_slugs:
+                queryset = queryset.filter(tags__slug=tag_slug)
+        queryset.distinct()
 
         return queryset
 
