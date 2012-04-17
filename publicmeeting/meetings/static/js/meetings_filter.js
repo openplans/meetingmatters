4ee85eee85fb3cb7ed2b1da69bd71de52a15d538
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-04-17 01:03:52 -0400

    Move the topic toggle functionality to its own javascript module

---
+++ publicmeeting/meetings/static/js/meetings_filter.js
@@ -0,0 +1,57 @@
+function initTopicFilter(tag) {
+  var $li = $('#id_tags-' + tag + '-li'),
+      $btn = $('#id_tags-' + tag + '-btn'),
+      $chk = $('#id_tags-' + tag);
+
+  if ($li.length !== 1)
+    console.error('The check box must be inside of a list item.');
+
+  if ($btn.length !== 1)
+    console.error('The check box should have a link to go with it.');
+
+  $btn.click(function() {
+    if ($chk.is(':checked')) {
+      $chk.removeAttr('checked');
+    } else {
+      $chk.attr('checked', 'checked');
+    }
+    $chk.trigger('change');
+  });
+
+  $chk.change(function() {
+    if ($chk.is(':checked')) {
+      $li.addClass('active');
+    } else {
+      $li.removeClass('active');
+    }
+  });
+}
+
+
+function initAllTopicsFilter() {
+  var $li = $('#id_all_tags-li'),
+      $btn = $('#id_all_tags-btn'),
+      $chk = $('input[name="tags"]'),
+      $form = $('#id_filter_form');
+
+  $btn.click(function() {
+    $chk.removeAttr('checked');
+    $chk.trigger('change');
+  });
+
+  $chk.change(function() {
+    if ($chk.filter(':checked').length > 0) {
+      $li.removeClass('active');
+    } else {
+      $li.addClass('active');
+    }
+  });
+
+  $chk.change(function() {
+    $form.submit();
+  })
+
+  $form.ajaxForm(function(responseText) {
+    alert(responseText);
+  });
+}
