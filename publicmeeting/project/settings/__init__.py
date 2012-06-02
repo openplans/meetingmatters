d2fb47ff5035e2fcbd132b6afdb79e00fa0d83dc
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-06-01 20:10:53 -0400

    Add default db settings to (hopefully) appease travis

--- publicmeeting/project/settings/__init__.py
+++ publicmeeting/project/settings/__init__.py
@@ -81,12 +81,9 @@ MANAGERS = ADMINS
 
 DATABASES = get_local('DATABASES', {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'project',
+        'USER': 'postgres',
     }
 })
 
